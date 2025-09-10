172344b34e2cce242287e5538153b84fcd05212d
SHAHID-BOOP <mohdshahida891@gmail.com>
2025-09-10 22:50:42 +0530

    Added input validation using zod

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,13 +2,21 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import { CreateUSerSchema } from "@repo/common/types";
+import { CreateUSerSchema,CreateRoomSchema,SigninSchema } from "@repo/common/types";
 
 const app = express();
 app.use(express.json());
 
 app.post("/signup", (req,res) => {
     // db call
+
+    const data = CreateUSerSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return
+    }
     res.json({
         userId: 123
     })
@@ -16,6 +24,13 @@ app.post("/signup", (req,res) => {
 });
 
 app.post("/signin", (req,res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return
+    }
 
     const userId = 1;
     const token = jwt.sign({
@@ -29,6 +44,14 @@ app.post("/signin", (req,res) => {
 
 app.get("/room", middleware, (req,res) => {
     //db call 
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        });
+        return
+    };
+
     res.json({
         roomId: 123
     })
