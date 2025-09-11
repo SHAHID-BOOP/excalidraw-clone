c039176279db253fbeaa16a1363580bafcdaa3ae
SHAHID-BOOP <mohdshahida891@gmail.com>
2025-09-11 10:48:15 +0530

    Added signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,24 +3,37 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import { CreateUSerSchema,CreateRoomSchema,SigninSchema } from "@repo/common/types";
-import {client} from "db";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
 app.use(express.json());
 
-app.post("/signup", (req,res) => {
+app.post("/signup",async (req,res) => {
     // db call
     
-    const data = CreateUSerSchema.safeParse(req.body);
-    if(!data.success) {
+    const parsedData = CreateUSerSchema.safeParse(req.body);
+    if(!parsedData.success) {
         res.json({
             message: "Incorrect inputs"
         })
         return
     };
-    res.json({
-        userId: 123
-    });
+    try{
+        await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.username,
+                password: parsedData.data.password,
+                name: parsedData.data?.name  
+            }  
+        })
+        res.json({
+            userId: "               "
+        });
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists with this username"
+        })
+    };
 
 });
 
